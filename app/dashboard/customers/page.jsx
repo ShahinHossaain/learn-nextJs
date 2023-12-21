376bdc6f8288e54e538d3d28975b65f2b7f71de6
ShahinHossaain <mdshahinhossaain@gmail.com>
2023-12-21 12:45:04 +0600

    first commit

---
+++ app/dashboard/customers/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <>
+            Customers page
+        </>
+    );
+};
+
+export default page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <>Invoices page</>;
+};
+
+export default page;
